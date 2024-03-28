407840c9b42af825bd5c3d07e9b191ab8cf79b53
SAHIL HOSSAIN <111786720+Sahil1786@users.noreply.github.com>
2024-03-29 02:50:28 +0530

    linkedlist qus added

--- Linklist/src/LL.java
+++ Linklist/src/LL.java
@@ -4,99 +4,142 @@
 
 public class LL {
 
-    private  Node head;
+    private static Node head;
     private Node tail;
-    private  int size;
+
+    private int size;
+
     public LL() {
-        this.size=0;
-    }
-
-    public void insertLast(int val){
-        if (tail==null){
-            insertFist(val);
-            return;
-        }
-        Node node=new Node(val);
-        tail.next=node;
-        tail =node;
-        size++;
-    }
-
-    public void insert(int val , int index){
-        if (index==0){
-            insertFist(val);
-            return;
-        }
-        if (index==size){
-            insertLast(val);
-            return;
-        }
-        Node temp=head;
-        for (int i = 1; i < index; i++) {
-            temp=temp.next;
-        }
-        Node node=new Node(val,temp.next);
-  temp.next=node;
-
-        size++;
-    }
-    public Node get(int index){
-        Node node =head;
-        for (int i = 0; i < index; i++) {
-            node= node.next;
-        }
-        return node;
+        this.size = size;
     }
 
 
-    public int deleteLst(){
-        if (size<=1){
-            return  deleitFist();
-        }
-        Node SecondLast=get(size-2);
-        int val =tail.value;
-        tail=SecondLast;
-        tail.next=null;
-        return  val;
-    }
-    public  int deleitFist(){
-        int val =head.value;
-        head=head.next;
-        if (head==null){
-            tail=null;
-        }
-        size--;
-        return  val;
-    }
-    public void dispaly(){
-        Node temp=head;
-        while (temp !=null){
-            System.out.print(temp.value + "--> ");
-            temp=temp.next;
-        }
-        System.out.println("END");
-    }
-       public void insertFist(int val ){
-        Node node=new Node(val);
+
+
+
+    public void insertFirst(int val ){
+        Node node =new Node(val);
         node.next=head;
         head=node;
         if (tail==null){
             tail=head;
         }
         size+=1;
-       }
-    private  class Node{
+    }
 
-        private  int value;
+
+    public  void insertLast(int val){
+        if (tail==null){
+            insertLast(val);
+            return;
+        }
+            Node node=new Node(val);
+            tail.next=node;
+            tail=node;
+            size++;
+
+    }
+
+
+    public void insert(int val,int index){
+        if (index==0){
+            insertFirst(val);
+            return;
+        }
+        if(index==size){
+            insertLast(val);
+            return;
+
+        }
+
+        Node temp=head;
+        for (int i = 1; i <index ; i++) {
+            temp=temp.next;
+        }
+        Node node=new Node(val,temp.next);
+        temp.next=node;
+        size++;
+    }
+    public  void display(){
+        Node temp= head;
+        while (temp !=null){
+            System.out.print(temp.value + "-> " );
+            temp=temp.next;
+        }
+        System.out.println("end");
+    }
+
+
+
+    public int delete(int index){
+        if (index ==0){
+            return deleteFirst();
+
+        }
+        if (index==size-1){
+            return  deleteLast();
+        }
+        Node pre =get(size=1);
+        int val = pre.next.value;
+        pre.next=pre.next.next;
+        return val;
+    }
+
+
+    public int deleteLast(){
+        if(size<=1){
+           return deleteFirst();
+        }
+        Node secoundLaast=get(size-2);
+        int  val = tail.value;
+        tail=secoundLaast;
+        tail.next=null;
+        return val;
+    }
+
+
+    public  Node find(int value ){
+        Node node=head;
+       while(node!=null){
+           if(node.value ==value){
+                return node;
+           }
+           node=node.next;
+       }
+        return null;
+    }
+
+
+    public  Node get(int index ){
+        Node node=head;
+        for (int i = 0; i <index ; i++) {
+            node=node.next;
+        }
+        return  node;
+    }
+
+    public int deleteFirst() {
+        int val =head.value;
+        head=head.next;
+        if (head==null) {
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+
+    private class Node{
+        private int value;
         private Node next;
 
         public Node(int value) {
             this.value = value;
         }
+
         public Node(int value, Node next) {
             this.value = value;
             this.next = next;
         }
-
     }
 }
--- Linklist/src/Main.java
+++ Linklist/src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+
+
+
+public class Main {
+    public static void main (String args []){
+        LL list =new LL();
+        list.insertFirst(3);
+        list.insertFirst(9);
+
+        list.insertFirst(4);
+        list.insertLast(99);
+        list.insert(100,2);
+        list.display();
+        System.out.println(list.deleteFirst());
+         list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+    }
+
+
 }
